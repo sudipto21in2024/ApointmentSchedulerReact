0bd71a49a358b8ac424ded4d026686845db87650
sudipto21in2024 <sudipto21in2024@outlook.com>
2025-10-01 12:11:20 +0530

    Deleted angular project

--- frontend/AppointmentSaas/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  API_BASE_URL: '/api',
-  DEBUG_MODE: false,
-};
--- frontend/AppointmentSaas/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  API_BASE_URL: 'http://localhost:3000/api',
-  DEBUG_MODE: true,
-};
